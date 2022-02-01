8065e6dff65dd73c6c0283ffb3265e7e3b030cb0
Francesco <cicciodb@hotmail.it>
2022-02-01 23:26:14 +0100

    fake_data_stream.csv created

--- big-faker-creation/writeFakeDataStreams.js
+++ big-faker-creation/writeFakeDataStreams.js
@@ -8,9 +8,7 @@ const outputFile = `${__dirname}\\csv2upload\\${streamFile}`;
 function writeFakeDataStreams () {
   const myWriteStream = fs.createWriteStream(outputFile);
   for (let i=0; i<=70; i++) {
-      for (let j=0; j<=100000; j++) {
-        myWriteStream.write(getFakeOrder());
-      }
+      myWriteStream.write(getFakeOrder());
     }
 }
 
