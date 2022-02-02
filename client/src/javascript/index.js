c1d1ad318b8b438bd1a1b7532c9242cff4d40cb8
Tommaso <tommasovenza@gmail.com>
2022-02-03 00:42:02 +0100

    client server upload

--- client/src/javascript/index.js
+++ client/src/javascript/index.js
@@ -1,1 +1,28 @@
-console.log("Hi");
+const axios = require("axios")
+const input = document.querySelector("#input")
+
+input.addEventListener("change", (e) => {
+  const dataToSend = e.target.files[0]
+  const formData = new FormData()
+
+  formData.append("file", dataToSend)
+
+  callBackend(formData)
+})
+
+// func to call backend
+function callBackend(formData) {
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  }
+  axios
+    .post("http://localhost:3000/upload", formData, config)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
