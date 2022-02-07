1ea787f64fb89a0171bde47b409d5beec12c2f21
sfdev25 <sfdev25@gmail.com>
2022-02-07 19:27:00 +0100

    [FIX] minor fixes and add comment on code

--- Auth0-API-test/app.js
+++ Auth0-API-test/app.js
@@ -6,30 +6,35 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const csv = require('@fast-csv/parse');
 
-async function readCSVwriteData(){
-    let counter = 0; 
+let invalidInserts = [] // an array where data of a failed insert will be populated 
+var counter = 1  // this is the counter of csv row number we will be reading
 
-// let header = [];
-// let data = [];
-let csvStream = await csv.parseFile(".\\csv\\fake_data_stream.csv", { headers: true, delimiter: ";" })
-    .on("data", function(record){
-        csvStream.pause();
-        if (counter < 1000) {
-            insertData(record)
+async function readCSVwriteData() { // function used to parse the csv file and write in the db
+    console.log(new Date().toLocaleString());
+
+    // let header = [];
+    // let data = [];
+    let csvStream = await csv.parseFile(".\\csv\\fake_data_stream_copy.csv", {
+            headers: true,
+            delimiter: ";"
+        })
+        .on("data", async (record) => {
+            csvStream.pause();
             ++counter;
-        }
+            await insertData(record)
+            csvStream.resume();
 
-        csvStream.resume();
-
-    }).on("end", function(){
-        console.log("Job is done!");
-    }).on("error", function(err){
-        console.log(err);
-    });
+        }).on("end", () => {
+            console.log(invalidInserts)
+            console.log("Job is done!");
+            console.log(new Date().toLocaleString());
+        }).on("error", (err) => {
+            console.log(err);
+        });
 }
 
 
-async function queryAll() {
+async function queryAll() { // function used to query all the data in the db
     const allOrders = await prisma.order.findMany({
         include: {
             user: true,
@@ -39,36 +44,45 @@ async function queryAll() {
     return allOrders
 }
 
-async function insertData(record) {
-    await prisma.order.create({
-        data: {
-        orderDate: new Date('2022-01-31T22:53:30.333'),
-        externalId: record.orderId,
-        quantity: parseInt(record.orderQuantity),
-        price: parseFloat(record.orderPrice),
-        user: {
-            create: {
-                firstname: record.firstName,
-                lastname: record.lastName,
-                email: record.email,
-                address: record.address,
-                zip: parseInt(record.zipCode),
-                city: record.city,
-                country: record.country,
-                phone: record.phone,
-                username: record.userName
-            },
-        },
-        product: {
-            create: {
-                name: record.productName,
-                productType: record.productType,
-                color: record.productColor,
-                description: record.productDescr
-            },
-        },
+async function insertData(record) { // function used to insert csv row data in the db
+    try {
+        await prisma.order.create({
+            data: {
+                orderDate: new Date(record.orderDate),
+                externalId: record.orderId,
+                quantity: parseInt(record.orderQuantity),
+                price: parseFloat(record.orderPrice),
+                user: {
+                    create: {
+                        firstname: record.firstName,
+                        lastname: record.lastName,
+                        email: record.email,
+                        address: record.address,
+                        zip: parseInt(record.zipCode),
+                        city: record.city,
+                        country: record.country,
+                        phone: record.phone,
+                        username: record.userName
+                    },
+                },
+                product: {
+                    create: {
+                        name: record.productName,
+                        productType: record.productType,
+                        color: record.productColor,
+                        description: record.productDescr
+                    },
+                },
+            }
+        })
+    } catch (e) {
+        console.log("Insert Function Error:")
+        console.log(e)
+        // in the following line we insert the row number and the record that generated the error inside the invalidInserts array
+        invalidInserts.push(counter, record)
     }
-})
+
+
 }
 
 var jwtCheck = jwt({
@@ -92,24 +106,24 @@ app.get("/public", (req, res) => {
 
 app.get("/queryAll", jwtCheck, async (req, res) => {
     const query = await queryAll()
-    .catch((e) => {
-        throw e
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+        .catch((e) => {
+            throw e
+        })
+        .finally(async () => {
+            await prisma.$disconnect()
+        })
     res.json(query)
 })
 
 app.get("/insert", jwtCheck, (req, res) => {
-    
+
     readCSVwriteData()
-    .catch((e) => {
-        throw e
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
+        .catch((e) => {
+            throw e
+        })
+        .finally(async () => {
+            await prisma.$disconnect()
+        })
 })
 
 app.listen(5000)
