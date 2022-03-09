839f7b47a584ac3993954a84b4d1c6025b946964
Angela Busato <busato.angela@gmail.com>
2022-03-09 07:56:17 +0100

    fix: faker creation file and server insert query

--- big-faker-creation/writeFakeDataStreams.js
+++ big-faker-creation/writeFakeDataStreams.js
@@ -1,17 +1,25 @@
-const fs = require('fs-extra');
+const { ro } = require("faker/lib/locales");
+const fs = require("fs-extra");
 const { getFakeOrder } = require("./getFakeOrders");
-const streamFile = 'fake_data_stream.csv';
-const outputFile = `${__dirname}\\csv2upload\\${streamFile}`;
+const streamFile = "fake_data_stream.csv";
+const outputFile = `${__dirname}/csv2upload/${streamFile}`;
 
+const fileHeader =
+  "First Name;Last Name;Email;Address;Zip;City;Country;Phone;Username;Order Date;Order ID;Quantity;Product;Type;Color;Description;Price\n";
 
-
-function writeFakeDataStreams () {
-  const myWriteStream = fs.createWriteStream(outputFile);
-  for (let i=0; i<=150000; i++) {
+function writeFakeDataStreams(rowNumbers) {
+  const myWriteStream = fs.createWriteStream(outputFile, { flags: "a" });
+  myWriteStream.write(fileHeader);
+  for (let i = 1; i <= rowNumbers; i++) {
+    if (i === Number(rowNumbers)) {
+      const lastRow = getFakeOrder().slice(0, -1);
+      myWriteStream.write(lastRow);
+    } else {
       myWriteStream.write(getFakeOrder());
     }
+  }
 }
 
 module.exports = {
-    writeFakeDataStreams
-}
+  writeFakeDataStreams,
+};
