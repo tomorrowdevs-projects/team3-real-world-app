f05ac1a17f4cb7081f57695784c5ab0145324263
Angela Busato <busato.angela@gmail.com>
2022-03-09 08:41:04 +0100

    feat: add append fake data creation file

--- big-faker-creation/index.js
+++ big-faker-creation/index.js
@@ -1,4 +1,5 @@
 const { writeFakeDataStreams } = require("./writeFakeDataStreams");
+const { appendFake } = require("./appendFakeData");
 
 const readline = require("readline").createInterface({
   input: process.stdin,
@@ -7,7 +8,7 @@ const readline = require("readline").createInterface({
 
 readline.question("How many rows do you want to create? ", (rowNumbers) => {
   console.log(`Ok row numbers ${rowNumbers}`);
-  writeFakeDataStreams(rowNumbers);
+  appendFake(rowNumbers);
 
   readline.close();
 });
